e8fd0bf66942103b02a65d5e66e8e7b140ec6179
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-10-09 13:44:38 +0200

    Add input for adding tasks and some button CSS

--- src/components/Tareas.vue
+++ src/components/Tareas.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="tarea in tareas" :key="tarea.id">
-                {{ tarea.title }}
-                <button @click="eliminarTarea(tarea.id)">x</button>
-            </li>
-        </ul>
+    <div class="inputDiv">
+        <input type="text" v-model="title" name="title" placeholder="¿Qué tienes que hacer?" @keyup.enter="addTarea" v-focus>
+        <button class="agregar" ><font-awesome-icon icon="plus" /></button>
+        <div>
+            <ul>
+                <li v-for="tarea in tareas" :key="tarea.id">
+                    {{ tarea.title }}
+                    <button class="eliminar" @click="eliminarTarea(tarea.id)"><font-awesome-icon icon="times" class="iconPosition"/></button>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimes, faPlus } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+library.add(faTimes, faPlus);
+
 export default {
 name: "Tareas.vue",
+    components: {
+        FontAwesomeIcon
+    },
     computed: {
         tareas() {
             return this.$store.getters.getTodos;
@@ -29,5 +42,49 @@ name: "Tareas.vue",
 </script>
 
 <style scoped>
+
+.inputDiv {
+    /*display: flex;*/
+    /*text-align: center;*/
+    margin-left: 32%;
+    padding-bottom: 20px;
+}
+
+input {
+    background: transparent;
+    border: solid 1px;
+    padding: 7px 10px;
+    border-right: none;
+    width: 40%;
+    color: black;
+    border-color: black;
+    font-size: 22px;
+}
+
+.agregar {
+    border: solid 1px;
+    padding: 7px 10px;
+    background: paleturquoise;
+    cursor: pointer;
+    width: 10%;
+    color: black;
+    font-size: 22px;
+    text-align: center;
+    vertical-align: center;
+}
+
+.eliminar {
+    margin-left: 20px;
+    border-radius: 0;
+    height: 20px;
+    width: 20px;
+    background-color: #afeeee;
+    cursor: pointer;
+}
+
+.iconPosition {
+    position: relative;
+    right: 2.5px;
+}
 
 </style>
