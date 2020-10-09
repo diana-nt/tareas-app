93c7966f839ff1d0c61f62081d8e695ce5bea62b
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-10-09 12:18:47 +0200

    Add unversioned files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <h2>TAREAS QUE HACER</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
+h2 {
+
+    font-family: 'Oswald', sans-serif;
+    text-align: center;
+    padding-top: 50px;
+    font-size: 60px;
+    text-decoration: underline;
+    text-decoration-color: paleturquoise;
+}
+</style>
