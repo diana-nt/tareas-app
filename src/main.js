3d1be3a905abf75b32647f4af0685bd6553b0757
Diana Munteanu <diana.munteanu@nodrizatech.com>
2020-11-19 09:39:21 +0100

    Start testing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './store/index.js'
 import router from './router.js'
-import { i18n } from './plugins/i18n'
+import { i18n } from './plugins/i18n.js'
 
 
 Vue.config.productionTip = false
